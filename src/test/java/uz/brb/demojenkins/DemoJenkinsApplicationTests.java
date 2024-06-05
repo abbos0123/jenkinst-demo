0f421f79d5540d38cf51c208e8a8f39e62a2b460
Abbos <abbosfayziboev@gmail.com>
2024-06-05 10:26:18 +0500

    uploading project

---
+++ src/test/java/uz/brb/demojenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.brb.demojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
